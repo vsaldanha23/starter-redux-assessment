0b0646ff1e30b3e8a5d2a2efbcf80bd2a41d87b6
vsaldanha <vsaldanha@indeed.com>
2023-12-14 18:24:27 -0800

    Redux Assessment: Add/Remove/Search dog

--- src/features/suggestion/index.js
+++ src/features/suggestion/index.js
@@ -5,11 +5,13 @@ import {
   selectError,
   selectLoading,
   // Task 18: Import the `selectSuggestion()` selector from the suggestion slice
+  selectSuggestion
 } from './suggestion.slice';
 import './suggestion.css';
 
 export default function Suggestion() {
   // Task 19: Call useSelector() with the selectSuggestion() selector
+  const {caption, imageUrl} = useSelector(selectSuggestion);
   // The component needs to access the `imageUrl` and `caption` properties of the suggestion object.
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
@@ -18,6 +20,8 @@ export default function Suggestion() {
   useEffect(() => {
     async function loadSuggestion() {
       // Task 20: Dispatch the fetchSuggestion() action creator
+      dispatch(fetchSuggestion());
+
     }
     loadSuggestion();
   }, [dispatch]);
@@ -31,8 +35,12 @@ export default function Suggestion() {
     // Task 21: Enable the two JSX lines below needed to display the suggestion on the page
     render = (
       <>
-        {/* <img alt={caption} src={imageUrl} />
-        <p>{imageUrl}</p> */}
+        { 
+        <div>
+        <img alt={caption} src={imageUrl} />
+        <p>{imageUrl}</p> 
+        </div>
+        }       
       </>
     );
   }
